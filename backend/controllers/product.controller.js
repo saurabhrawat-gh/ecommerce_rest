679ff863b67eaddf9c4c0efd36d1b228a755da3f
Saurabh Rawat <saurabhrawat.official@gmail.com>
2023-03-08 20:17:57 +0530

    dev: removed controller code from model to controller file

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,6 @@
+exports.getAllProducts = (req, res, next) => {
+    res.status(200).json({
+      message: "Route is working fine",
+    });
+  };
+  
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getAllProducts } = require('../models/product.model')
+const { getAllProducts } = require('../controllers/product.controller')
 
 const router = express.Router()
 
